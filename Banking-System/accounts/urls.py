b56254812b384b7e92244bd45a9f98aa388fe36a
tyagivipin04 <52811626+tyagivipin04@users.noreply.github.com>
2021-02-08 20:06:43 +0530

    Update urls.py

--- Banking-System/accounts/urls.py
+++ Banking-System/accounts/urls.py
@@ -1,15 +1,21 @@
 from django.urls import path
 
-from .views import DepositMoneyView, WithdrawMoneyView, TransactionRepostView
-from .views import export_csv
+from .views import UserRegistrationView, LogoutView, UserLoginView
 
-app_name = 'transactions'
 
+app_name = 'accounts'
 
 urlpatterns = [
-    path("deposit/", DepositMoneyView.as_view(), name="deposit_money"),
-    path("report/", TransactionRepostView.as_view(), name="transaction_report"),
-    path("withdraw/", WithdrawMoneyView.as_view(), name="withdraw_money"),
-    path("report/export_csv/", export_csv),
-
+    path(
+        "login/", UserLoginView.as_view(),
+        name="user_login"
+    ),
+    path(
+        "logout/", LogoutView.as_view(),
+        name="user_logout"
+    ),
+    path(
+        "register/", UserRegistrationView.as_view(),
+        name="user_registration"
+    ),
 ]
